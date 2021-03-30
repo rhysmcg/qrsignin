cb48a1ef04ac143198bc5f21643ab4ab2ec50ef0
rhysmcg <rhysmcgoldrick@gmail.com>
2021-03-30 10:23:19 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request
-from flask_socketio import SocketIO, send ##I also needed eventlet
+from flask_socketio import SocketIO, send
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
@@ -28,5 +29,6 @@ def sessions():
     return render_template('main.html', ip_address=request.remote_addr)
 
 if __name__ == '__main__':
-	socketio.run(app, host="0.0.0.0")
+	port = int(os.environ.get("PORT", 5000))
+	socketio.run(app, host="0.0.0.0",port=port)
 
